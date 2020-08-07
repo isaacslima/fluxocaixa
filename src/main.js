a791e38dd662af097aa4eabbc36d3319986d0efe
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-08-06 22:14:56 -0300

    adding main dependencies

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase';
+import './firebase'
+import money from 'v-money'
+Vue.use(money, { precision: 2 })
+
+import router from './router';
 
 Vue.config.productionTip = false
 
-new Vue({
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+firebase.auth().onAuthStateChanged(function () {
+  if (!app) {
+    app = new Vue({
+      firebase,
+      router,
+      vuetify,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
